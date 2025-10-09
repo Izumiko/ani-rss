9fbc59dd4ff4521dd0cf787f2d398559a2c7fdd7
Izumiko <im@cst.moe>
2025-10-09 17:04:28 +0800

    chore: 减小docker镜像体积
    - 删除sqlite中的native目录
    - 删除基础镜像中不必要的包 （alpine中ca-certificates  musl-locales musl-locales-lang tzdata无用）

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -e
 
 chown -R ${PUID}:${PGID} /usr/app
 
 umask ${UMASK}
 
-exec su-exec ${PUID}:${PGID} /run.sh
+exec su-exec "$PUID:$PGID" "$@"
--- docker/run.sh
+++ docker/run.sh
@@ -7,37 +7,7 @@ FOLDER="./"
 JAR_FILE_NAME="ani-rss.jar"
 JAR_FILE=$FOLDER$JAR_FILE_NAME
 
-if [ ! -f $JAR_FILE ]; then
-    URL="https://github.com/wushuo894/ani-rss/releases/latest/download/ani-rss.jar"
-    wget -O $JAR_FILE $URL
-
-    if [ $? -eq 0 ]; then
-        echo "$JAR_FILE 下载成功！"
-    else
-        echo "$JAR_FILE 下载失败。"
-    fi
-fi
-
-stop() {
-  PID=$(pgrep -f "$JAR_FILE_NAME")
-  if [ -n "$PID" ]; then
-      echo "Stopping process $PID - $JAR_FILE_NAME"
-      kill "$PID"
-      wait "$PID"
-  fi
-}
-
-stop
-
-sigterm_handler() {
-    stop
-}
-
-trap 'sigterm_handler' 15
-
-while :
-do
-    java -Xms60m -Xmx1g -Xss256k \
+exec java -Xms60m -Xmx1g -Xss256k \
       -Dfile.encoding=UTF-8 \
       -Xgcpolicy:gencon \
       -Xshareclasses:none \
@@ -52,10 +22,3 @@ do
       --add-opens=java.base/java.net=ALL-UNNAMED \
       --add-opens=java.base/sun.net.www.protocol.https=ALL-UNNAMED \
       -jar $JAR_FILE&
-    wait $!
-    if [ $? -ne 0 ]; then
-      break
-    fi
-done
-
-exit 0
